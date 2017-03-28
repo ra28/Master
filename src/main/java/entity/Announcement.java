84d936e352358ac06fd57c4f5ee00320944e17aa
Ana Lukic <analukic.ns@gmail.com>
2017-03-28 20:36:38 +0200

    Adding properties for domain objects.

--- src/main/java/entity/Announcement.java
+++ src/main/java/entity/Announcement.java
@@ -1,5 +1,97 @@
 package entity;
 
-public class Announcement {
+import java.awt.Image;
+import java.io.Serializable;
+import java.sql.Time;
+import java.util.Date;
+
+public class Announcement implements Serializable{
+	
+	private int idAnnouncement;
+	private String comment;
+	private Date date;
+	private Time time;
+	private int idShelter;
+	private Image imageUrl;
+	
+	
+	public Announcement() {
+		super();
+	}
+
+
+	public Announcement(int idAnnouncement, String comment, Date date, Time time, int idShelter, Image imageUrl) {
+		super();
+		this.idAnnouncement = idAnnouncement;
+		this.comment = comment;
+		this.date = date;
+		this.time = time;
+		this.idShelter = idShelter;
+		this.imageUrl = imageUrl;
+	}
+
+
+	public int getIdAnnouncement() {
+		return idAnnouncement;
+	}
+
+
+	public void setIdAnnouncement(int idAnnouncement) {
+		this.idAnnouncement = idAnnouncement;
+	}
+
+
+	public String getComment() {
+		return comment;
+	}
+
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+
+	public Date getDate() {
+		return date;
+	}
+
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+
+	public Time getTime() {
+		return time;
+	}
+
+
+	public void setTime(Time time) {
+		this.time = time;
+	}
+
+
+	public int getIdShelter() {
+		return idShelter;
+	}
+
+
+	public void setIdShelter(int idShelter) {
+		this.idShelter = idShelter;
+	}
+
+
+	public Image getImageUrl() {
+		return imageUrl;
+	}
+
+
+	public void setImageUrl(Image imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	
+	
+	
+	
 
 }
---
+++ src/main/java/entity/Gender.java
@@ -0,0 +1,7 @@
+package entity;
+
+public enum Gender {
+	
+	Male, 
+	Female	
+}
